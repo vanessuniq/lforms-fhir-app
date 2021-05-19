758086579abca9d22e8063cd9e97ca8f420cdfa8
khlukanets <khristina.lukanets@hevelian.com>
2021-05-19 14:19:15 +0300

    Change path for convert request

--- app/fhir-app/fhir-app-content.controller.js
+++ app/fhir-app/fhir-app-content.controller.js
@@ -128,7 +128,7 @@ angular.module('lformsApp')
             qr.questionnaire = qData.url;
           }
 
-          jQuery.ajax('http://localhost:8080/administration/convert', {
+          jQuery.ajax('http://localhost:8080/administration/$extract', {
             method: 'POST',
             crossDomain: true,
             contentType: 'application/json',
